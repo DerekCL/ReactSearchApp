77a68181c775cc771ee3c773a29b6499a145e3e2
Derek Lewandowski <derekclewandowski@gmail.com>
2018-09-02 00:45:41 -0500

    creating register action

--- src/ducks/app/actions.ts
+++ src/ducks/app/actions.ts
@@ -3,12 +3,14 @@ import { FSA } from "@src/helpers/FSA";
 /** Action type constants. */
 export const SWITCH_PAGE = "SWITCH_PAGE";
 export const LOGIN = "LOGIN";
+export const REGISTER = "REGISTER";
 
 /** Action type definitions. */
 export type SwitchPageAction = FSA<typeof SWITCH_PAGE, string>;
 export type LoginAction = FSA<typeof LOGIN>;
+export type RegisterAction = FSA<typeof REGISTER>;
 
-export type Action = SwitchPageAction | LoginAction;
+export type Action = SwitchPageAction | LoginAction | RegisterAction;
 
 /** Action creators.  */
 export function SwitchPage(page: string): SwitchPageAction {
